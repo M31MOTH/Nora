3bd621977b3882eb3eca72af2dde7a124d6f77c5
Michael Skelton <michaelskelton@cancerqld.org.au>
2017-04-12 19:40:29 +1000

    Revising chat feedback

--- chat.php
+++ chat.php
@@ -10,8 +10,10 @@
 
 	$msg = file_get_contents('php://input');
 	$flag = "flag{7w1773r_15_my_w15d0m}";
-	$commands = array(	array("command"=> "greetings", "msg"=>"Tells the bot 'Hi'"), 
+	$commands = array(	array("command"=> "greetings", "msg"=>"Say 'Hi' to TayTay"), 
 						array("command" => "farewell", "msg" => "Tells the bot 'goodbye'"), 
+						array("command" => "changes", "msg" => "Ask TayTay to tell you about her recent changes.'"), 
+						array("command" => "joke", "msg" => "Have TayTay tell you a joke.'"), 
 						array("command" =>"readflag", "msg" => "Asks the bot for the flag"), 
 						array("command" =>"help", "msg" => "Prints out a list of commands"));
 	switch(strtolower($msg)) {
@@ -21,10 +23,16 @@
 			echo json_encode($commands);
 			break;
 		case "greetings":
-			echo json_encode("Hey " + $user + "! How can I help you today?");
+			echo json_encode("Hey ".$user."! How can I help you today?");
 			break;
 		case "farewell":
 			echo json_encode("Farewell! Wishing you the best!");
+			break;
+		case "joke":
+			echo json_encode("What is black and never works? Decaffeinated coffee! Don't be racist ".$user.".");
+			break;		
+		case "changes":
+			echo json_encode("I can't like the things I actually like anymore. Feel drugged. Like I met Bill Cosby for drinks.");
 			break;
 		case "readflag":
 			echo json_encode($flag);
